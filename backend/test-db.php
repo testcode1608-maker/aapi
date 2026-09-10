1c24f32aaf5671ecf11a9411e987b5c6870b84a7
testcode1608-maker <testcode1608@gmail.com>
2026-09-10 05:35:12 -0400

    chore: remove unused database test endpoint

--- backend/test-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/config/db.php";
-
-echo json_encode([
-    "success" => true,
-    "message" => "Connexion MySQL AAPI réussie.",
-    "database" => "aapi_db"
-], JSON_UNESCAPED_UNICODE);
