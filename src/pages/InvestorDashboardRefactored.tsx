1e3ebf6add5ba7141f79f4c1269a56d0ee71786b
testcode1608-maker <testcode1608@gmail.com>
2026-09-14 06:30:49 -0400

    Load investor dashboard extra styles before layout fixes

--- src/pages/InvestorDashboardRefactored.tsx
+++ src/pages/InvestorDashboardRefactored.tsx
@@ -5,6 +5,7 @@ import "../styles/investor-dashboard.css";
 import "../styles/investor-dashboard-dark.css";
 import "../styles/investor-projects.css";
 import "../styles/investor-investments.css";
+import "../styles/investor-dashboard-extra.css";
 import "../styles/investor-dashboard-fixes.css";
 import InvestorDashboardSidebar from "../components/investor/InvestorDashboardSidebar";
 import InvestorDashboardTopbar from "../components/investor/InvestorDashboardTopbar";
