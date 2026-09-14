eb7d66451fe1536c40cfbedd0b5b0ef468b70ca2
testcode1608-maker <testcode1608@gmail.com>
2026-09-14 08:59:37 -0400

    refactor Home imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,12 @@
-import Hero from "../components/Hero";
-import Services from "../components/Services";
-import Sectors from "../components/Sectors";
-import Statistics from "../components/Statistics";
-import Opportunities from "../components/Opportunities";
-import Events from "../components/Events";
+import Hero from "../components/home/Hero";
+import Services from "../components/home/Services";
+import Sectors from "../components/home/Sectors";
+import Statistics from "../components/home/Statistics";
+import Opportunities from "../components/home/Opportunities";
+import Events from "../components/home/Events";
 
 function Home() {
-  return (
-    <>
-      <Hero />
-      <Services />
-      <Sectors />
-      <Statistics />
-      <Opportunities />
-     
-      <Events />
-    </>
-  );
+  return <><Hero /><Services /><Sectors /><Statistics /><Opportunities /><Events /></>;
 }
 
 export default Home;
